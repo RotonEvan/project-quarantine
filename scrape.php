f3eff18ba20dc1709c14113a447aa7bd601fe11b
saksham291 <sakshamb@iitbhilai.ac.in>
2020-03-25 21:22:32 +0530

    backend completed

--- scrape.php
+++ scrape.php
@@ -9,17 +9,36 @@ $page = curl_exec($curl);
 
 //$states = array();
 
-preg_match_all('/<td align="\'centre" width="83" valign="bottom">(?P<name>[^"]*)<\/td>\s+<td align="\'centre" width="91" valign="bottom">(?P<indian>[^"]*)<\/td>\s+<td align="\'centre" width="90" valign="top">(?P<foreign>[^"]*)<\/td>\s+<td align="\'centre" width="83" valign="top">(?P<cured>[^"]*)<\/td>\s+<td align="\'centre" width="83" valign="top">(?P<death>[^"]*)<\/td>/s', $page, $matches);
+preg_match_all('/<td align="\'centre" width="83" valign="bottom">(?P<name>[^"]*)<\/td>\s+<td align="\'centre" width="91" valign="bottom">(?P<indian>[^"]*)<\/td>\s+<td align="\'centre" width="90" valign="top">(?P<foreign>[^"]*)<\/td>\s+<td align="\'centre" width="83" valign=".*?">(?P<cured>[^"]*)<\/td>\s+<td align="\'centre" width="83" valign="top">(?P<death>[^"]*)<\/td>/s', $page, $matches);
 
-echo "<pre>".print_r($matches,true);
+//echo "<pre>".print_r($matches,true);
+//preg_match_all('/<td align="\'centre" width="83" valign="bottom">(?P<name>[^"]*)<\/td>\s+<td align="\'centre" width="91" valign="bottom">(?P<indian>[^"]*)<\/td>\s+<td align="\'centre" width="90" valign="top">(?P<foreign>[^"]*)<\/td>\s+<td align="\'centre" width="83" valign="bottom">(?P<cured>[^"]*)<\/td>\s+<td align="\'centre" width="83" valign="top">(?P<death>[^"]*)<\/td>/s', $page, $matches1);
 
-
+preg_match('/<td align="\'centre" width="91" valign="bottom"><strong>(?P<indian>[^"]*)<\/strong><\/td>\s+<td align="\'centre" width="90" valign="top">\s+<strong>(?P<foreign>[^"]*)<\/strong>\s+<\/td>\s+<td align="\'centre" width="83" valign="top">\s+<strong>(?P<cured>[^"]*)<\/strong>\s+<\/td>\s+<td align="\'centre" width="83" valign="top">\s+<strong>(?P<death>[^"]*)<\/strong>\s+<\/td>/s',$page, $total);
+$tot = (int)$total['indian'] + (int)$total['foreign'] + (int)$total['cured'] + (int)$total['death'];
+//echo "Total cases in India: ".$tot."<br> Indian Nationals:".$total['indian']."<br> Foreign Nationals:".$total['foreign']."<br> Cured:".$total['cured']."<br> Deaths:".$total['death'];
 if(curl_errno($curl)) // check for execution errors
 {
 	echo 'Scraper error: ' . curl_error($curl);
 	exit;
 }
 
-curl_close($curl);
-die;
+include('states_code.php');
 
+	for($i=0;$i<24;$i++)
+	{
+		$state_total[$i] = (int)$matches[2][$i]+(int)$matches[3][$i]+(int)$matches[4][$i]+(int)$matches[5][$i];
+		$state_active[$i] = (int)$matches[2][$i]+(int)$matches[3][$i];
+		$states[$i] = "<br/><span>Total Cases: ".$state_total[$i]."<br/>Active Cases: ".$state_active[$i]." <br/>Cured: ".$matches[4][$i]." <br/>Deaths: ".$matches[5][$i]."</span>";
+
+	}
+	$states[30] = "<br/><span>Total Cases: 0<br/>Active Cases: 0 <br/>Cured: 0 <br/>Deaths: 0</span>";
+
+	$response = json_encode($matches);
+
+	$fp = fopen('results.json', 'w');
+	fwrite($fp, json_encode($response));
+	fclose($fp);
+	//echo $response;
+
+curl_close($curl);
---
+++ states_code.php
@@ -0,0 +1,41 @@
+<?php
+$AP = 0;
+$bihar = 1;
+$cgh = 2;
+$del = 3;
+$guj = 4;
+$kar = 7;
+$keral = 8;
+$MP = 9;
+$mah = 10;
+$mani = 11;
+$odi = 13;
+$pudu = 14;
+$punjab = 15;
+$raj = 16;
+$TN = 17;
+$telan = 18;
+$ladakh = 20;
+$UP = 21;
+$utt = 22;
+$WB = 23;
+$har = 5;
+$HP = 6;
+$WB = 19;
+$tri = 30;
+$nag =30;
+$sik =30;
+$miz =12;
+$megh =30;
+$lak =30;
+$jnk =30;
+$jhar =30;
+$goa = 30;
+$dnh =30;
+$dnd =30;
+$chandi =19;
+$assam =30;
+$arun =30;
+$ani =30;
+
+?>
